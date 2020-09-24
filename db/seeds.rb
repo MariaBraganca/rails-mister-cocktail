685f8749db52e9d41dd55b9a84fbd2a9966ef822
MariaBraganca <mcpbbraganca@gmail.com>
2020-09-24 13:02:34 +0200

    seed ingredient case sensitive

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ chars.each do |character|
     (1..15).each do |i|
       next if drink["strIngredient#{i}"].nil? || drink["strMeasure#{i}"].nil?
       next if drink["strIngredient#{i}"].blank? || drink["strMeasure#{i}"].blank?
-      ingredient = Ingredient.where(name: drink["strIngredient#{i}"]).first_or_create!
+      ingredient = Ingredient.where(name: drink["strIngredient#{i}"].capitalize).first_or_create!
       dose = Dose.create!(
         cocktail_id: cocktail.id,
         ingredient_id: ingredient.id,
